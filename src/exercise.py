9e566adc84c02b289803d002a15475cc99beda27
Jake Lee <jake.lee@fdmgroup.com>
2021-10-02 22:44:13 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -1,6 +1,18 @@
 def main():
     #write your code below this line
     numbers = []
+    numbers.append(3)
+    numbers.append(2)
+    numbers.append(6)
+    numbers.append(-1)
+    numbers.append(5)
+    numbers.append(1)
+
+    print("The numbers in the range [0, 5]")
+    print_numbers_in_range(numbers, 0, 5)
+
+    print("The numbers in the range [3, 10]")
+    print_numbers_in_range(numbers, 3, 10)
 
 def print_numbers_in_range(numbers, lower_limit, upper_limit):
     for num in numbers:
